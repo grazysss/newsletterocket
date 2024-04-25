abc13ff0614e436fe80f885b83c4d3a6d4ebeff0
Grazielly Souza <graziellysouza2580@gmail.com>
2024-04-25 15:50:34 -0300

    :smile: update commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+    alert('Successfully registered E-mail!')
+}
+
+function touchEvent(event) {
+    const html = document.documentElement
+
+    html.classList.toggle("hidden");
+}
